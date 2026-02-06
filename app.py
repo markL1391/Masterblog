b43f9a82e2f9038d7871bce821b8dac471568c8f
Mark Lünnemann <Mark@MacBook-Air.local>
2026-02-06 13:59:29 +0100

    Add blog posts JSON data structure

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
+import json
 from flask import Flask
 
 app = Flask(__name__)
 
+POSTS_FILE = "posts.json"
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+def index():
+    # add code here to fetch the job posts from a file
+    return render_template('index.html', posts=blog_posts)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
